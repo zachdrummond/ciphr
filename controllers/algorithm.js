65469a38d462df00b3537cf24e965a77acddc385
Zach Drummond <zachdrummond3@gmail.com>
2020-11-05 14:45:05 -0500

    Added routes

---
+++ controllers/algorithm.js
@@ -0,0 +1,26 @@
+const router = express.Router();
+
+// Get all algorithms
+router.get("/api/algorithm", function (request, response) {
+
+});
+
+// Get a specific algorithm
+router.get("/api/algorithm/:id", function (request, response) {
+
+});
+
+// Create an algorithm
+router.post("/api/add/algorithm/:id", function(request, response) {
+
+});
+
+// Edit an algorithm
+router.put("/api/edit/algorithm/:id", function(request, response) {
+
+});
+
+// Delete an algorithm
+router.delete("/api/delete/algorithm/:id", function(request, response) {
+
+});
