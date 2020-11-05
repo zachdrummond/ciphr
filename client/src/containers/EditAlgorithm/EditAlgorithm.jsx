81de176ed84e037041cc5a4b625b95700c45b112
Calvin Griffin <cgriffin332@gmail.com>
2020-11-05 14:10:02 -0500

    added basic containers

---
+++ client/src/containers/EditAlgorithm/EditAlgorithm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditAlgorithm = () => {
+    return (
+        <div>
+            <h1>Edit Algorithm Page</h1>
+        </div>
+    );
+};
+
+export default EditAlgorithm;
