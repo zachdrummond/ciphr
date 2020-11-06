52b65460be38abbe4a07da3b448b20b89caecfb1
Zach Drummond <zachdrummond3@gmail.com>
2020-11-06 11:49:27 -0500

    Added then and catch blocks

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,18 +3,30 @@ const router = express.Router();
 const db = require("../models");
 
 // SIGNUP ROUTE
-router.post("/api/signup", (request, response) => {
-    if(!request.body.username.trim() || !request.body.password.trim()) {
-        response.status(400); // Bad Request
-    } else {
-        db.Users.create({
-            username: request.body.username,
-            password: request.body.password
+router.post("/api/signup", ({ username, password }, response) => {
+  // Validation
+  if (!username.trim() || !password.trim()) {
+    response.status(400); // Bad Request
+  } else {
+    // Create a new user
+    db.Users.create({
+      username: username,
+      password: password,
+    })
+      .then((newUser) => {
+        response.json(newUser);
+      })
+      .catch((error) => {
+        console.log(error);
+        response.status(500).json({
+          error: true,
+          data: null,
+          message: "Unable to sign up",
         });
-    }
-})
+      });
+  }
+});
 
 // LOGIN ROUTE
-
 
 module.exports = router;
