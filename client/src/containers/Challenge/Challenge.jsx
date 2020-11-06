4e166a9959cef9ab7c7e72e5b5969f1fbbe32a1c
Andrew Stewart <1andrewstewart@gmail.com>
2020-11-06 11:49:37 -0500

    basic grid for challenge ui

--- client/src/containers/Challenge/Challenge.jsx
+++ client/src/containers/Challenge/Challenge.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(8, 4),
+  },
+}));
 
 const Challenge = () => {
-    return (
-        <div>
-            <h1>Challenge Page</h1>
-        </div>
-    );
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.root}>
+      <Typography variant="h4" color="textSecondary" align="left">
+        Challenge: The Three Comma Club
+      </Typography>
+      <Grid item xs={12}>
+        <Grid container>
+          <Grid item xs={6}>
+            Column 1
+          </Grid>
+          <Grid item xs={6}>
+            Column 2
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Challenge;
