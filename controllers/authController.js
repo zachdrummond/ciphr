94e5fb961743539162da0d9b2a982f5289c06986
Zach Drummond <zachdrummond3@gmail.com>
2020-11-06 11:49:56 -0500

    Added bcrypt with hashedPassword

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 const db = require("../models");
 
@@ -8,20 +9,33 @@ router.post("/api/signup", ({ username, password }, response) => {
   if (!username.trim() || !password.trim()) {
     response.status(400); // Bad Request
   } else {
-    // Create a new user
-    db.Users.create({
-      username: username,
-      password: password,
-    })
-      .then((newUser) => {
-        response.json(newUser);
+    // Hash the password
+    bcrypt
+      .hash(password, 10)
+      .then((hashedPassword) => {
+        // Create a new user
+        db.Users.create({
+          username: username,
+          password: hashedPassword,
+        })
+          .then((newUser) => {
+            response.json(newUser);
+          })
+          .catch((error) => {
+            console.log(error);
+            response.status(500).json({
+              error: true,
+              data: null,
+              message: "Unable to sign up",
+            });
+          });
       })
       .catch((error) => {
         console.log(error);
         response.status(500).json({
           error: true,
           data: null,
-          message: "Unable to sign up",
+          message: "Unable to hash password",
         });
       });
   }
