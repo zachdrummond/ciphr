e4ea1a8e8ed9998c5255a27ca1aa9dcdcd12d6dd
Calvin Griffin <cgriffin332@gmail.com>
2020-11-06 11:14:51 -0500

    add navLinks

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 // import * as React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
+import { Home } from "@material-ui/icons";
 
-const navlinks = [
+const navLinks = [
   { title: `home`, path: `/home` },
   { title: `Logout`, path: `/` },
   { title: `Add Algorithm`, path: `/algorithms/new` },
@@ -11,7 +19,20 @@ const navlinks = [
 const Header = () => {
   return (
     <AppBar position="static">
-      <Toolbar></Toolbar>
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="home">
+          <Home fontSize="large" />
+        </IconButton>
+        <List component="nav" aria-labelledby="main navigation">
+          {navLinks.map(({ title, path }) => (
+            <a href={path} key={title}>
+              <ListItem button>
+                <ListItemText primary={title} />
+              </ListItem>
+            </a>
+          ))}
+        </List>
+      </Toolbar>
     </AppBar>
   );
 };
