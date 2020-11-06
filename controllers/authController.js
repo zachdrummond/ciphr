62d1ae9323b4dac083cc38645fe4cf87ae67b382
Zach Drummond <zachdrummond3@gmail.com>
2020-11-06 11:48:28 -0500

    Added validation to post request

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
+// SIGNUP ROUTE
 router.post("/api/signup", (request, response) => {
-    
+    if(!request.body.username.trim() || !request.body.password.trim()) {
+        response.status(400); // Bad Request
+    }
 })
+
+// LOGIN ROUTE
 
 
 module.exports = router;
