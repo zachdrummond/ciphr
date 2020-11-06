071dbc82dccab20b94b0f0e1f2b52abca0a7f22a
Zach Drummond <zachdrummond3@gmail.com>
2020-11-06 11:47:58 -0500

    Added sign up post request

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../models");
+
+router.post("/api/signup", (request, response) => {
+    
+})
 
 
 module.exports = router;
