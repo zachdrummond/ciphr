fe8d149f4b3ebd6718bf7542d51f11a422e3358d
Calvin Griffin <cgriffin332@gmail.com>
2020-11-09 00:41:09 -0500

    use Login code

--- client/src/containers/SignUp/SignUp.jsx
+++ client/src/containers/SignUp/SignUp.jsx
@@ -1,11 +1,93 @@
-import React from 'react';
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+// import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import API from "../../utils/API"
+import CredentialsForm from "../../components/CredentialsForm/CredentialsForm"
 
-const SignUp = () => {
-    return (
-        <div>
-            <h1>Sign Up Page</h1>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage: "url(./img/magic-macbook.jpg)",
+    // backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignInSide() {
+  const classes = useStyles();
+
+  // hook configures username/password state
+  // to find state in dev tools 'Components' look under 'SignInSide'
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    // handles input of either username or password
+    setUserInfo({...userInfo, [name]: value} );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // username/password posted to back end
+    // see API.js in utils for more info
+    console.log(userInfo)
+    API.postUserInfo(userInfo).then((response) => {
+      console.log(response);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={8} className={classes.image} />
+      <Grid item xs={12} sm={8} md={4} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <CredentialsForm {...userInfo} handleInput={handleInput} handleSubmit={handleSubmit} classes={classes} type={"Sign up"} link={"/"} linkText={"Already have an account? Sign in"}/>
+          
         </div>
-    );
-};
-
-export default SignUp;
+      </Grid>
+    </Grid>
+  );
+}
