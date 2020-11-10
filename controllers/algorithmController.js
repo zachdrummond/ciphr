eff4adb8f23bd14e909aafda6178c6fb4c621c35
Calvin Griffin <cgriffin332@gmail.com>
2020-11-10 11:56:52 -0500

    got username from jwt on add algo page

--- controllers/algorithmController.js
+++ controllers/algorithmController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
+const jwt = require("jsonwebtoken");
 
 // Get all algorithms
 router.get("/api/algorithm", function (request, response) {
@@ -22,8 +23,8 @@ router.get("/api/algorithm", function (request, response) {
 // Get a specific algorithm
 router.get("/api/algorithm/:id", function (request, response) {
   // response.json({ success: "Get a single algorithm worked!" });
-  db.Algorithms.findOne({_id:request.params.id}).
-  populate('testCases')
+  db.Algorithms.findOne({ _id: request.params.id })
+    .populate("testCases")
     .then((algorithm) => {
       response.json(algorithm);
     })
@@ -41,31 +42,44 @@ router.get("/api/algorithm/:id", function (request, response) {
 //TODO: Add user info to algorithm model
 router.post("/api/algorithm", (req, res) => {
   console.log(req.body);
-  const { testCases, algorithm } = req.body;
-  // first test cases are created from the front end input (can be empty array!)
-  db.TestCases.insertMany(testCases)
-    .then((testCaseResponse) => {
-      // then an algorithm entry is created with the front end input and test cases from db
-      db.Algorithms.create({
-        challengeName: algorithm.challengeName,
-        description: algorithm.description,
-        testCases: testCaseResponse,
-      }).then((newAlgorithm) => {
-        res.status(200).json({
-          error: false,
-          data: newAlgorithm,
-          message: "Successfully posted new algorithm.",
-        });
-      });
-    })
-    .catch((err) => {
+  const { testCases, algorithm, userJwt } = req.body;
+  const decoded = jwt.verify(userJwt, process.env.SECRET, (err, decoded) => {
+    if (err) {
       console.log(err);
-      res.status(500).json({
+      return response.status(401).json({
         error: true,
         data: null,
-        message: "Failed to create algorithm.",
+        message: "Invalid token.",
       });
-    });
+    } else {
+      console.log(decoded);
+    }
+  });
+  // first test cases are created from the front end input (can be empty array!)
+  //   db.TestCases.insertMany(testCases)
+  //     .then((testCaseResponse) => {
+  //       // then an algorithm entry is created with the front end input and test cases from db
+  //       db.Algorithms.create({
+  //         challengeName: algorithm.challengeName,
+  //         description: algorithm.description,
+  //         testCases: testCaseResponse,
+
+  //       }).then((newAlgorithm) => {
+  //         res.status(200).json({
+  //           error: false,
+  //           data: newAlgorithm,
+  //           message: "Successfully posted new algorithm.",
+  //         });
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       res.status(500).json({
+  //         error: true,
+  //         data: null,
+  //         message: "Failed to create algorithm.",
+  //       });
+  //     });
 });
 
 // Edit an algorithm
