2f3b75ae3eed996509e5291795bac5dbce122b3c
Zach Drummond <zachdrummond3@gmail.com>
2020-11-10 18:06:58 -0500

    Added comments

--- client/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,9 +2,12 @@ import { Redirect, Route } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../../context/AuthContext/AuthContext";
 
+// Takes in a component and all of its props
 const ProtectedRoute = ({ component: Component, ...rest }) => {
+  // Destructures jwt from the AuthContextAPI
   const { jwt } = useContext(AuthContext);
 
+  // If the user has logged in or signed up, the user can access the website. Otherwise, the user is redirected to the login page.
   return (
     <Route
       {...rest}
