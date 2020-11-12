92810e85d8888eceb6e699aaba4f65a8cf941841
Zach Drummond <zachdrummond3@gmail.com>
2020-11-12 11:05:39 -0500

    Merge issue

--- controllers/algorithmController.js
+++ controllers/algorithmController.js
@@ -22,10 +22,6 @@ router.get("/api/algorithm", function (request, response) {
 
 //Get my algorithms
 router.get("/api/algorithm/user/:userJwt", function (request, response) {
-<<<<<<< HEAD
-  // console.log(request.params.userJwt);
-=======
->>>>>>> main
   jwt.verify(request.params.userJwt, process.env.SECRET, (err, decoded) => {
     if (err) {
       console.log(err);
