d3951f16ef52b3880cd3c69e7385527562280d15
Calvin Griffin <cgriffin332@gmail.com>
2020-11-11 20:54:35 -0500

    fixed a couple yellow warnings and modal message

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 // React
-import { useContext } from "react";
+// import { useContext } from "react";
 import { Link } from "react-router-dom";
 // Material UI
 import {
@@ -17,7 +17,7 @@ import { Home } from "@material-ui/icons";
 import Brightness4Icon from "@material-ui/icons/Brightness4";
 import Brightness7Icon from "@material-ui/icons/Brightness7";
 // File Modules
-import AuthContext from "../../context/AuthContext/AuthContext";
+// import AuthContext from "../../context/AuthContext/AuthContext";
 
 const useStyles = makeStyles({
   toolbar: {
@@ -40,7 +40,7 @@ const navLinks = [
 const Header = ({ theme, setTheme }) => {
   const classes = useStyles();
     // Using AuthContextAPI to get the setJwt function
-    const { jwt } = useContext(AuthContext);
+    // const { jwt } = useContext(AuthContext);
 
   const changeMode = () => {
     !theme ? setTheme(true) : setTheme(false);
