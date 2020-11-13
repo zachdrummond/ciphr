2bbdedc42fb69712cbefc4abff4e80322adc0f8d
Zach Drummond <zachdrummond3@gmail.com>
2020-11-13 09:49:53 -0500

    Finished editing algorithms with hashtags

--- client/src/containers/AddAlgorithm/AddAlgorithm.jsx
+++ client/src/containers/AddAlgorithm/AddAlgorithm.jsx
@@ -88,6 +88,7 @@ export default function AddAlgorithm() {
       }
     }
 
+    // Convert the hashtags to an array
     const hashtagArray = algoInfo.hashtags.match(/#\w+/g);
 
     API.addAlgorithm({
