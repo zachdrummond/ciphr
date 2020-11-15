24bf902641f4ebd980c7eb91ef05e413415972a9
Andrew Stewart <1andrewstewart@gmail.com>
2020-11-14 22:03:32 -0500

    saving prior to updating app.js

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -8,29 +8,29 @@ import {
 
 const useStyles = makeStyles({
   appBar: {
-    top: 'auto',
+    top: "auto",
     bottom: 0,
   },
 });
 
 export default function Footer() {
   const classes = useStyles();
-  
+
   return (
-    <AppBar className={classes.appBar} position="fixed" color="primary">
-        <Toolbar>
-          <Box margin="auto">
+    <AppBar className={classes.appBar} position="absolute" color="primary">
+      <Toolbar>
+        <Box margin="auto">
           <Typography
             align="center"
             position="static"
             variant="body1"
             color="inherit"
           >
-            Ciphr &copy; 2020 -- Zach Drummond, Calvin Griffin, Joseph
-            Perry, Andrew Stewart
+            Ciphr &copy; 2020 -- Zach Drummond, Calvin Griffin, Joseph Perry,
+            Andrew Stewart
           </Typography>
-          </Box>
-        </Toolbar>
+        </Box>
+      </Toolbar>
     </AppBar>
   );
 }
