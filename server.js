e855b00f93c7aaad9203a9ce9fe7b59f71a1411f
Joseph Perry <josephperry720@gmail.com>
2021-02-17 15:13:11 -0500

    cleaned up code

--- server.js
+++ server.js
@@ -13,15 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("client/build"));
 app.use(cookieParser());
-app.use(
-  jwt({
-    secret: process.env.SECRET,
-    getToken: (req) => req.cookies.token,
-    algorithms: ['HS256'],
-  }).unless({
-    path: ["/login", "/signup", "/", "/api/login", "/api/token"],
-  })
-);
 app.use(require("./controllers/algorithmController.js"));
 app.use(require("./controllers/authController.js"));
 app.use(require("./controllers/codeController"));
@@ -39,20 +30,6 @@ mongoose.connect(
 );
 
 const connection = mongoose.connection;
-
-// app.get("/api/token", jwt({
-//     secret: process.env.SECRET,
-//     algorithms: ['HS256'],
-//   }), (req, res) => {
-//     try {
-//       const token = req.cookies.token;
-//       console.log("here")
-//       console.log(token)
-//       res.json(token);
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   });
 
 connection.on("connected", () => {
   console.log("Mongoose successfully connected.");
